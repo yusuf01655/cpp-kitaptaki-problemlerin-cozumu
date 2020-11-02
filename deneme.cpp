4dd9d8280c5f0a90870ff6a7ea30d3232a348e28
yusuf01655 <54513789+yusuf01655@users.noreply.github.com>
2020-11-03 02:22:59 +0300

    Update deneme.cpp

--- deneme.cpp
+++ deneme.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
-using namesp
+using namespace std;
+class SanalSinif{
+protected:
+    string isim;
+public:
+  SanalSinif(string _isim):isim(_isim){};
+  virtual string isimDondur() = 0;
+};
+class TelevizyonAcKomutu :public SanalSinif{
+public:
+  TelevizyonAcKomutu() :SanalSinif("Televizyon"){}
+  string isimDondur();
+};
+string TelevizyonAcKomutu::isimDondur(){ return isim;}
+
+int main(){}
