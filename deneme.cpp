abda005601416cf92cb88b3bc8b529ee57cb7d72
yusuf01655 <54513789+yusuf01655@users.noreply.github.com>
2020-11-04 02:35:16 +0300

    Update deneme.cpp

--- deneme.cpp
+++ deneme.cpp
@@ -13,5 +13,51 @@ public:
   string isimDondur();
 };
 string TelevizyonAcKomutu::isimDondur(){ return isim;}
+class TelevizyonKapaKomutu :public SanalSinif{
+public:
+    TelevizyonKapaKomutu():SanalSinif("Televizyon"){};
+    string isimDondur();
+};
+string TelevizyonKapaKomutu::isimDondur(){ return isim;}
+class OtoparkKapiAcKomutu :public SanalSinif {
+public:
+    OtoparkKapiAcKomutu():SanalSinif("Otopark"){}
+    string isimDondur();
+};
+string OtoparkKapiAcKomutu::isimDondur() {
+    return isim;
+}
+class OtoparkKapiKapaKomutu :public SanalSinif{
+    public:
+    OtoparkKapiKapaKomutu :SanalSinif("Otopark"){}
+    string isimDondur();
+};
+string OtoparkKapiKapaKomutu::isimDondur(){ return isim; }
+class UzaktanKumanda{
+    SanalSinif* acDizi[1];
+    SanalSinif* kapaDizi[1];
+public:
+    void ac();
+    void kapa();
+    void acmaKomutuAta(SanalSinif* nsn);
+    void kapamaKomutuAta(SanalSinif* nsn);
+};
+void UzaktanKumanda::ac(){
+    SanalSinif* e = acDizi[0];
+    cout << e->isimDondur() << " aciliyor..." << endl;
+}
+void UzaktanKumanda::kapa(){
+    SanalSinif* e = kapaDizi[0];
+    cout << e->isimDondur() << "kapaniyor...";
+}
+void UzaktanKumanda::acmaKomutuAta(SanalSinif* nsn){
+    acDizi[0]=nsn;
+}
+void UzaktanKumanda::kapamaKomutuAta(SanalSinif* nsn){
+    kapaDizi[0]=nsn;
+}
+int main(){
 
-int main(){}
+
+return 0;
+}
